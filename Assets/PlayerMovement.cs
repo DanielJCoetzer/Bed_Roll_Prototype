64ffae90d3c82f48466b276b7f7d599fd1aaa7b9
Daniel Coetzer <djcoetzer@gmail.com>
2024-10-23 22:52:12 +0200

    First Commit
    Added grid movement and Bedroom Scene. Also added physics to push furniture

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    PlayerInput playerInput;
+    Vector2 direction;
+    InputAction moveAction;
+    private bool isMoving = false;
+    private GameObject furniture = null;
+    private IEnumerator MoveCoroutine;
+
+    private float count = 6f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        /*if (isMoving == true) {
+            count = count -0.1f;
+            if (count <= 0) {
+                isMoving = false;
+                count = 6f;
+            }
+        }*/
+        if (isMoving == false) {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            {
+                MovePlayer();
+                isMoving = true;            
+            }
+        }
+        /*if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)) {
+                isMoving = false;
+            }*/
+    }
+
+    void MovePlayer()
+    {
+        direction = moveAction.ReadValue<Vector2>();
+        if (direction.x != 0 && direction.y != 0){
+            direction.y = 0;
+        }
+        //transform.position += new Vector3();
+        //transform.Translate(direction.x, 0, direction.y);
+        StartMoveCoroutine(direction.x, direction.y);
+    }
+
+    private void StartMoveCoroutine(float directionX, float directionY = 0)
+    {
+        if (MoveCoroutine != null)
+        {
+            //StopCoroutine(MoveCoroutine);
+        }
+            MoveCoroutine = Move(directionX, directionY);
+            StartCoroutine(MoveCoroutine);
+
+        
+    }
+
+    private IEnumerator Move(float directionX, float directionY)
+    {
+        float blockDistance = 1f; // Adjust the block distance as needed
+        float elapsedTime = 0f;
+        float moveTime = 0.2f;
+
+        Vector3 startPosition = transform.position;
+        Vector3 endPosition = startPosition + new Vector3(directionX, 0 , directionY);
+
+        while (elapsedTime < moveTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / moveTime;
+            transform.position = Vector3.Lerp(startPosition, endPosition, t);
+            yield return null;
+        }
+
+        transform.position = endPosition;
+        isMoving = false;
+    }
+}
